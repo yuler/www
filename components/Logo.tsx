6a2c7f7efd335c38a5b42468021808c3a7cd9c3f
yuler <joyful4yu@gmail.com>
2020-05-16 00:32:02 +0800

    Remove useless files & add `Logo` component

---
+++ components/Logo.tsx
@@ -0,0 +1,20 @@
+
+const Logo = () => {
+  return (
+    <>
+    <div>Y</div>
+    <style jsx>{`
+      div {
+        width: 40px;
+        height: 40px;
+        color: white;
+        background: black;
+        text-align: center;
+        line-height: 40px;
+      }
+    `}</style>
+    </>
+  )
+}
+
+export default Logo
