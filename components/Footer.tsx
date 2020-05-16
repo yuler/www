2d075ca637a850de49ebd92253562f7340920be4
yuler <joyful4yu@gmail.com>
2020-05-16 22:17:42 +0800

    Design a simple home page

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => (
+  <footer>
+    <a href="https://github.com/yuler/www">[Source Code]</a>
+    <style jsx>{`
+      footer {
+        font-size: .8rem;
+      }
+      a {
+        color: #999;
+        text-decoration: none;
+      }
+    `}</style>
+  </footer>
+)
+
+export default Footer
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,23 +3,26 @@ import Head from 'next/head'
 
 type IProps = {
   title?: string
+  className?: string
 }
 
-const Layout: React.FC<IProps> = ({ children }) => {
+const Layout: React.FC<IProps> = ({ className, children }) => {
   return (
-    <main>
+    <div className={className}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       {children}
 
       <style jsx>{`
-        main {
+        div {
+          padding: 1rem;
           margin: 0 auto;
           max-width: 42rem;
+          min-height: 100vh;
         }  
       `}</style>
-    </main>
+    </div>
   )
 }
 
