7aff1c8f97f887380d82e124784b709090203c87
yuler <joyful4yu@gmail.com>
2020-06-14 17:56:31 +0800

    Add `posts/[slug]` page & Render it from `_posts` markdown file

--- utils/api.ts
+++ utils/api.ts
@@ -3,12 +3,13 @@ import path from 'path'
 import matter from 'gray-matter'
 
 const POSTS_DIR = path.join(process.cwd(), '_posts')
+const POST_DEFAULT_FIELDS = ['slug', 'title', 'content']
 
 export function getPostSlugs() {
   return fs.readdirSync(POSTS_DIR)
 }
 
-export function getPostBySlug(slug: string, fields: string[] = []) {
+export function getPostBySlug(slug: string, fields: string[] = POST_DEFAULT_FIELDS) {
   const realSlug = slug.replace(/\.md$/, '')
   const fullPath = path.join(POSTS_DIR, `${realSlug}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
@@ -33,7 +34,7 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
   return items
 }
 
-export function getAllPosts(fields: string[] = []) {
+export function getAllPosts(fields: string[] = POST_DEFAULT_FIELDS) {
   const slugs = getPostSlugs()
   return slugs.map(slug => getPostBySlug(slug, fields))
 }
